70d6206cffd6379e055de5a8c42437c3411af853
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-09-09 16:31:40 -0500

    add: typography

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col">
-        <p>
-          Bienvenido a la comunidad Superbeauty Negocios. Un lugar diseñado para
-          crecer tu negocio de belleza de manera sostenible.
-        </p>
+        <h2>Los más vendidos</h2>
       </div>
       <div class="bg-secondary col">COl 2</div>
       <div class="my-widget col">COl 3</div>
