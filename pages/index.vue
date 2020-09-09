7b18e8a818ee80a708f39ab3792c54640b06c159
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-09-09 15:29:22 -0500

    add:  Mixins and Handelson Font

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col">COl 1</div>
+      <div class="col">
+        <p>
+          Bienvenido a la comunidad Superbeauty Negocios. Un lugar diseñado para
+          crecer tu negocio de belleza de manera sostenible.
+        </p>
+      </div>
       <div class="bg-secondary col">COl 2</div>
       <div class="my-widget col">COl 3</div>
     </div>
