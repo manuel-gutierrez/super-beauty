5e655eb53a75902b16333d76a986dfae7a52a7ae
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-09-13 22:55:41 -0500

    add: tooltip to cart

--- components/Cart.vue
+++ components/Cart.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="d-inline-flex">
     <svg-icon class="cart-icon" icon="handbag-icon" />
+    <div class="counter mt-1"><div class="counter-number">10</div></div>
   </div>
 </template>
 
