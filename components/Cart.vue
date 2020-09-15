df8e6883188227b8881e6cbfb6b7ae2e8b19d808
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-09-15 14:38:44 -0500

    feat: add "Add to Cart" method

--- components/Cart.vue
+++ components/Cart.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="d-inline-flex">
     <svg-icon class="cart-icon" icon="handbag-icon" />
-    <div class="counter mt-1"><div class="counter-number">10</div></div>
+    <div class="counter mt-1">
+      <div class="counter-number">{{ counter }}</div>
+    </div>
+    <div @click="addToCart">add to cart</div>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters, mapMutations } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters('cart', { counter: 'getCartCounter' }),
+  },
+  methods: {
+    ...mapMutations('cart', ['incrementCartCounter']),
+    addToCart() {
+      this.incrementCartCounter();
+    },
+  },
+};
 </script>
 
 <style></style>
