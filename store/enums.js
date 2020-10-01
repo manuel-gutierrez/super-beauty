48633adcb6426fc7d811767ee6d52fdb046a9724
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-10-01 17:48:43 -0500

    add: data models and schemas

--- store/enums.js
+++ store/enums.js
@@ -1,18 +1,18 @@
 export const state = () => ({
-  filterNames: {
-    PRICE: 'price',
-    RATING: 'rating',
-    BRAND: 'brand',
-    VARIANT: 'variant',
-    OTHER: 'other',
-    SUSTAINABLE: 'sustainable',
-    NEW: 'new',
-  },
-});
-export const mutations = {};
+    filterNames: {
+        PRICE: "price",
+        RATING: "rating",
+        BRAND: "brand",
+        VARIANT: "variant",
+        OTHER: "other",
+        SUSTAINABLE: "sustainable",
+        NEW: "new",
+    },
+})
+export const mutations = {}
 
 export const getters = {
-  getEnum: (state) => (enumName) => {
-    return state[enumName];
-  },
-};
+    getEnum: (state) => (enumName) => {
+        return state[enumName]
+    },
+}
