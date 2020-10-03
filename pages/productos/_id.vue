caedbcc70f4154dfc790b14f88eb0f92aa7c464f
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-10-03 17:26:11 -0500

    add:  page

---
+++ pages/productos/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container product-detail-page">
+    <div class="row">
+      <div class="col-4 mr-auto"><</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters('login', {
+      isloggedIn: 'getCurrentLoginStatus',
+      sessionToken: 'getSessionToken',
+    }),
+    ...mapGetters('products', {
+      getProduct: 'getProductById',
+    }),
+    product() {
+      return this.getProduct(this.$route.params.id);
+    },
+  },
+};
+</script>
+
+<style></style>
