f117bb449a27790bda84555ebd2224d442209fa5
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-10-14 20:31:23 -0500

    add: progress bar checkout

---
+++ components/ProgressBar/ProgressBarCheckout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="checkout-progress-bar"></div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters('cart', {
+      freeShippingValue: 'getFreeShippingValue',
+      items: 'getCartItems',
+      totals: 'getTotals',
+    }),
+  },
+};
+</script>
+
+<style></style>
--- store/checkoutProgressBar.js
+++ store/checkoutProgressBar.js
@@ -4,8 +4,20 @@ export const state = () => ({
     step_1: 'DATOS DE ENVÍO',
     step_2: 'FINALIZAR PEDIDO',
   },
+  activeStep: 0,
+  numberOfSteps: 3,
 });
-export const mutations = {};
+export const mutations = {
+  nextStep(state) {
+    state.activeStep++;
+  },
+  previousStep(state) {
+    state.activeStep--;
+  },
+  resetStep(state) {
+    state.step = 0;
+  },
+};
 export const actions = {
   // For future use.
 };
@@ -13,5 +25,11 @@ export const actions = {
 export const getters = {
   getLabels: (state) => {
     return state.labels;
+  },
+  getNumberOfSteps: (state) => {
+    return state.numberOfSteps;
+  },
+  getActiveStep: (state) => {
+    return state.activeStep;
   },
 };
