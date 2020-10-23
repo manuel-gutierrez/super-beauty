b7dfbb66c4c1e13bf02c79387c44fede8bf447a7
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-10-23 11:10:26 -0500

    add: search bar

--- components/SearchModal.vue
+++ components/SearchModal.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="container fluid">
     <b-modal id="search-modal" size="full" hide-footer no-stacking>
+      <template v-slot:modal-title class="search-modal__header">
+        <b-icon-search />
+        <b-form-input v-model="text" placeholder="Búsqueda"></b-form-input>
+      </template>
       this is the search modal
     </b-modal>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      text: '',
+    };
+  },
+};
 </script>
 
 <style></style>
