8ac57f1ca50a7f0f974200e2d12cfa041b83c08b
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-10-23 10:33:59 -0500

    add: position and background color

---
+++ components/SearchModal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container fluid">
+    <b-modal id="search-modal" size="full" hide-footer no-stacking>
+      this is the search modal
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
