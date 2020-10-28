951ff90cdc54b9bb65a8a7dfd0b06e1d103aa924
Manuel Gutiérrez <m.gutierrez@druo.com>
2020-10-28 12:33:42 -0500

    feat: community main page

---
+++ pages/comunidad.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container community-page">
+    <div class="community-page__banner">
+      <BannerImage
+        :data="banner.data"
+        :type="banner.type"
+        class="col-12 px-md-0"
+      />
+    </div>
+    <div class="community-page__intro">
+      <h2>{{ intro.title }}</h2>
+      <p v-html="intro.content"></p>
+    </div>
+    <div class="community-page__sections">
+      <div class="community-page__digital-content">
+        <b-img
+          :alt="digitalContent.image.caption"
+          :src="digitalContent.image.src"
+        ></b-img>
+        <h2>{{ digitalContent.title }}</h2>
+        <p>{{ digitalContent.content }}</p>
+        <nuxt-link :to="digitalContent.button.url">
+          <button>{{ digitalContent.button.label }}</button>
+        </nuxt-link>
+      </div>
+      <div class="community-page__conversations">
+        <b-img
+          :alt="conversations.image.caption"
+          :src="conversations.image.src"
+        ></b-img>
+        <h2>{{ conversations.title }}</h2>
+        <p>{{ conversations.content }}</p>
+        <nuxt-link :to="conversations.button.url">
+          <button>{{ conversations.button.label }}</button>
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="section-spacer"></div>
+    <ScrollToTop />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters('pages/community', ['getCommunityContent']),
+    banner() {
+      return this.getCommunityContent('banner');
+    },
+    intro() {
+      return this.getCommunityContent('section_0');
+    },
+    digitalContent() {
+      return this.getCommunityContent('section_1');
+    },
+    conversations() {
+      return this.getCommunityContent('section_2');
+    },
+  },
+};
+</script>
+
+<style></style>
--- store/pages/community.js
+++ store/pages/community.js
@@ -3,6 +3,7 @@ export const state = () => ({
     banner: {
       type: 'hero',
       data: {
+        url: '#',
         image: {
           src: '/images/pages/community/banner.png',
           caption: 'Community banner image',
@@ -12,29 +13,35 @@ export const state = () => ({
     section_0: {
       title: 'Construyamos juntos algo hermoso',
       content:
-        'Bienvenido a la comunidad Superbeauty Negocios. Un lugar diseñado para crecer tu negocio de belleza de manera sostenible.',
+        'Bienvenido a la comunidad Superbeauty Negocios. </br> Un lugar diseñado para crecer tu negocio de belleza de manera sostenible.',
     },
     section_1: {
       title: 'Contenido digital',
       content: 'Descarga contenido para la promoción de tus productos.',
       image: {
-        img: '/images/pages/community/digital-content.png',
+        src: '/images/pages/community/digital-content.png',
         caption: 'Digital Content download section image',
       },
-      button: 'Mostrar Más',
+      button: {
+        label: 'Mostrar Más',
+        url: '/contenido-digital',
+      },
     },
     section_2: {
       title: 'Conversaciones',
       content:
         'Interactuara con gente como tu, emprendedora y apasionada por la belleza.',
       image: {
-        img: '/images/pages/community/digital-content.png',
+        src: '/images/pages/community/conversations.png',
         caption: 'Conversations section',
       },
-      button: 'Mostrar Más',
+      button: {
+        label: 'Mostrar Más',
+        url: '/conversaciones',
+      },
     },
   },
 });
 export const getters = {
-  getComunityContent: (state) => (sectionName) => state.sections[sectionName],
+  getCommunityContent: (state) => (sectionName) => state.sections[sectionName],
 };
